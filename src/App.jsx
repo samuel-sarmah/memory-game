a02633e86222becaa7c67b161afe5ebabf2b94fb
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-09-25 19:58:49 +0300

    Set up app component boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { useState } from "react"
+import Form from "./components/Form"
+import Card from "./components/Card"
 
 function App() {
+  const [isGameOn, setIsGameOn] = useState(false);
+
+  function startGame(e) {
+    e.preventDefault();
+    setIsGameOn(true)
+  }
+
+  function turnCard() {
+    console.log("Card has been clicked.")
+  }
+
   return (
     <>
-      <h1>Sarmah here!</h1>
+      <h1>Memory!</h1>
+      {isGameOn && <Form handleSubmit={startGame} />}
+      {isGameOn && <Card handleClick={turnCard} />}
     </>
   )
 }
