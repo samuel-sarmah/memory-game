54358b95052b39c2bff8b267fa853168ca35934d
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-09-25 19:54:19 +0300

    Boilerplate for regular button component

---
+++ src/components/RegularButton.jsx
@@ -0,0 +1,9 @@
+export default function RegularButton({ children, handleClick }) {
+    return (
+        <button
+            className="btn btn-text"
+            onClick={handleClick}>
+                {children}
+            </button>
+    )
+}
