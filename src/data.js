f6684cf52d32aef111117f1699fdf367fdae5b4c
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-09-25 20:14:03 +0300

    Create and set up data file

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const data = {
+    category: [
+        { 
+            name: "Animals and nature",
+            value: "animals-and-nature"
+        },
+        {
+            name: "Food and drink",
+            value: "food-and-drink"
+        },
+        {
+            name: "Objects",
+            value: "objects"
+        },
+        {
+            name: "Symbols",
+            value: "symbols"
+        }
+    ],
+    number: [
+        {
+            value: "10"
+        },
+        {
+            value: "20"
+        },
+        {
+            value: "30"
+        },
+        {
+            value: "40"
+        },
+    ]
+}
+
+export { data }
