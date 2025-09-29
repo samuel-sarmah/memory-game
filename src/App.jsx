db0b896e884ff1ca8f16fc30d207d8022af83550
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-09-29 22:32:41 +0300

    Update styles files

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Form from "./components/Form"
 import Card from "./components/Card"
 import AssistiveTechInfo from "./components/AssistiveTechInfo";
 import GameOver from "./components/GameOver";
+import './index.css'
 
 
 
