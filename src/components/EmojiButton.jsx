cbaff139a6492971547906372570d9278f61ac16
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-09-29 13:18:31 +0300

    Create and set up emoji button with card flipping logic

---
+++ src/components/EmojiButton.jsx
@@ -0,0 +1,23 @@
+export default function EmojiButton({
+    content,
+    handleClick,
+    selectedCardEntry,
+    matchedCardEntry
+}) {
+    
+    const btnContent = selectedCardEntry || matchedCardEntry ? content : "?"
+
+    const btnStyle =
+        matchedCardEntry ? "btn--emoji__back--matched" :
+        selectedCardEntry ? "btn--emoji__back--selected" :
+        "btn--emoji__front"
+
+    return (
+        <button
+            className={`btn btn--emoji ${btnStyle}`}
+            onClick={handleClick}
+        >
+            {btnContent}
+        </button>
+    )
+}
