a6c82d482b0df7d977146eb2963daf9cdf6f783d
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-09-29 22:13:48 +0300

    Create an implement game over component to display game end message

---
+++ src/components/GameOver.jsx
@@ -0,0 +1,9 @@
+export default function GameOver() {
+    return (
+        <div className="wrapper wrapper-accent">
+            <p className="p--large">
+                You've matched all the memory cards!
+            </p>
+        </div>
+    )
+}
