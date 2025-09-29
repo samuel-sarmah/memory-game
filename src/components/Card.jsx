31d73a52faa4f1ea6cf95ee4677acabcbae6fe40
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-09-29 13:50:06 +0300

    Update card component to handle card selection and matching

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,14 +2,25 @@ import { decodeEntity } from "html-entities"
 import EmojiButton from "./EmojiButton"
 
 export default function Card({ handleClick, data, selectedCards, matchedCards }) {
+    const cardEl = data.map((emoji, index) => {
+        const selectedCardEntry = selectedCards.find(emoji => emoji.index === index)
+        const matchedCardEntry = matchedCards.find(emoji => emoji.index === index)
 
-    const emojiEl = data.map((emoji, index) => 
-        <li key={index} className="card-item">
-            <button className="btn btn--emoji" onClick={() => handleClick(emoji.name, index)}>
-                {decodeEntity(emoji.htmlCode[0])}
-            </button>
-        </li>
-    )
+        const cardStyle =
+            matchedCardEntry ? "card-item--matched" :
+            selectedCardEntry ? "card-item--selected" : ""
+        
+        return (
+            <li key={index} className={`card-item ${cardStyle}`}>
+                <EmojiButton
+                    content={decodeEntity(emoji.htmlCode[0])}
+                    handleClick={() => handleClick(emoji.name, index)}
+                    selectedCardEntry={selectedCardEntry}
+                    matchedCardEntry={matchedCardEntry}
+                />
+            </li>
+        )
+    })
 
     return <ul className="card-container">{emojiEl}</ul>
 }
