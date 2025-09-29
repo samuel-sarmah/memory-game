04dd534120991105f651b9a597661a27b91cb1a6
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-09-29 21:42:57 +0300

    Hide assistive tech component from screen

--- src/components/AssistiveTechInfo.jsx
+++ src/components/AssistiveTechInfo.jsx
@@ -1,6 +1,6 @@
 export default function AssistiveTechInfo({ emojisData, matchedCards }) {
     return (
-        <section>
+        <section className="sr-only">
             <h2>Game status</h2>
             <p>Number of matched pairs: {matchedCards.length / 2}.</p>
             <p>Number of cards left: {emojisData.length - matchedCards.length}.</p>
