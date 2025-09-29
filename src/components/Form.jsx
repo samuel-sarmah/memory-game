fe941e380989c0e05f70cbcba64919f72c1ee142
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-09-30 00:22:30 +0300

    Add instructions on game play at start

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,10 +2,19 @@ import RegularButton from './RegularButton';
 
 export default function Form ({ handleSubmit }) {
     return (
-        <form className='wrapper'>
-            <RegularButton handleClick={handleSubmit}>
-                Start Game
-            </RegularButton>
-        </form>
+        <> 
+            <div className='wrapper--accent'>
+                <h2>Welcome to the Memory game!</h2> <br />
+                <p><b> Each game uses a random set of animal & nature emojis.</b> </p><br />
+                <p><b>Flip two cards at a time to find matching pairs. </b></p><br />
+                <p> <b>The game ends when all pairs are matched. </b></p> <br />
+                <p><em>Restart the game with a new set of emojis.</em></p>
+            </div>
+            <form className='wrapper'>
+                <RegularButton handleClick={handleSubmit}>
+                    Start Game
+                </RegularButton>
+            </form>
+        </>
     )
 }
