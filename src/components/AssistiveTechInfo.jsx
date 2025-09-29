5f45a1f1f726ff9d748f05d191b63e24353390e4
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-09-29 21:57:18 +0300

    feat: prompt assistive technology to present all content of section whenever part of it changes

--- src/components/AssistiveTechInfo.jsx
+++ src/components/AssistiveTechInfo.jsx
@@ -1,6 +1,6 @@
 export default function AssistiveTechInfo({ emojisData, matchedCards }) {
     return (
-        <section className="sr-only">
+        <section className="sr-only" aria-live="polite" aria-atomic="true">
             <h2>Game status</h2>
             <p>Number of matched pairs: {matchedCards.length / 2}.</p>
             <p>Number of cards left: {emojisData.length - matchedCards.length}.</p>
