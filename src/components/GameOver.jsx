80be7f6cf657c3ff01d03a095cd6373059b561db
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-09-29 23:45:54 +0300

    Add accessibility to game over component

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,9 +1,15 @@
 import RegularButton from "./RegularButton"
+import { useEffect, useRef } from "react"
 
 
 export default function GameOver({ handleClick }) {
+
+    const divRef = useRef(null);
+    useEffect(() => {
+        divRef.current.focus()
+    }, [])
     return (
-        <div className="wrapper wrapper--accent">
+        <div className="wrapper wrapper--accent" ref={divRef} tabIndex={-1}>
             <p className="p--large">
                 You've matched all the memory cards!
             </p>
